e6ffd3e610a93365227b3111f11dbe6e28af782e
Ebina-iphone <ohapi217208@icloud.com>
2026-04-06 13:41:48 +0900

    コミット

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'app-details',
@@ -9,5 +10,13 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  // 「route」という名前の変数（場所）を作って、そこに「URL専門家ActivatedRoute」（代入する）
+  //この一行を書いたあとの route という変数の中には、自分では作れない**「URLに関するあらゆる情報」**がギッシリ詰まっています。
+  route: ActivatedRoute =inject(ActivatedRoute);
+  housingLocationId =0;
+
+  constructor(){
+    this.housingLocationId=Number(this.route.snapshot.paramMap)
+  }
 
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -2,12 +2,13 @@ import { Component,Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
 import { DetailsComponent } from '../details/details.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css','../details/details.Component.css']
 })
